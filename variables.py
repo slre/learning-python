239cafe31c3bd6aececac87ba05b297c0d603cf0
Salvador <isc.slre@gmail.com>
2024-12-09 12:10:59 -0600

    python variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+myInt = 5
+print(myInt)
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+print("the type of mystring is ",type(mystring))
